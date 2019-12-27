4add43792cd24d341b40ad76231fa9d2d299d1f5
David Vivó <david.vivo@oramon.nl>
2019-12-27 11:30:34 +0100

    Refactor conditional into if statement

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const validationComponent = (props) => {
+    let textConfirmation = (
+        <p>Text too short</p>
+    );
+    
+    if (props.length >= 5) {
+        textConfirmation = (
+            <p>Tex long ennough</p>
+        );
+    }
+    
     return (
         <div>
             <p>The input's length is {props.length}</p>
-            { props.length < 5 ?
-                <div>
-                    <p>Text too short</p>
-                </div>
-            : null }
-            { props.length >= 5 ?
-                <div>
-                    <p>Text long ennough</p>
-                </div>
-            : null }
+            {textConfirmation}
         </div>
     );
 }
